d48c7bdaf0b18ecd6b496f00f733240fde3487a6
shijito <zero.seiyuu@gmail.com>
2023-01-16 01:24:59 +0900

    profile CSS　改

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -10,50 +10,50 @@
     <form action="/profile" method="post" enctype="multipart/form-data"><!--enctype=”multipart/form-data”は画像を送信する際のおまじない-->
       @csrf
       <table class="profileupdate">    
-        <tr>
+        <tr class="username-form">
           <!--username-->
           @if($errors->has('username'))  <!--has()でエラー文の1つを指定して表示できる-->
             {{ $errors->first('username') }}
           @endif 
-          <th>user name</th>
-          <td><input type="text" class="text" name="username" value="{{$user->username}}"></td><!--Usercontroller　profileでuser情報を取得してその中からusernameのみを取得。userテーブルの中のカラム情報を取り出している-->
+          <th class="user-name">user name</th>
+          <td><input class="profile-input" type="text" class="text" name="username" value="{{$user->username}}"></td><!--Usercontroller　profileでuser情報を取得してその中からusernameのみを取得。userテーブルの中のカラム情報を取り出している-->
         </tr>
-        <tr>
+        <tr class="mail-form">
           <!--mail-->
           @if($errors->has('mail'))  <!--has()でエラー文の1つを指定して表示できる-->
             {{ $errors->first('mail') }}
           @endif 
-          <th>mail adress</th>
-          <td><input type="text" name="mail" value="{{$user->mail}}"></td><!--Usercontroller　profileでuser情報を取得してその中からmailのみを取得-->
+          <th class="mail-adress">mail adress</th>
+          <td><input class="profile-input" type="text" name="mail" value="{{$user->mail}}"></td><!--Usercontroller　profileでuser情報を取得してその中からmailのみを取得-->
         </tr>
-        <tr>
+        <tr class="password-form">
           <!--password-->
           @if($errors->has('newpassword'))  <!--has()でエラー文の1つを指定して表示できる-->
             {{ $errors->first('newpassword') }}
           @endif 
-          <th>password</th>
-          <td><input type="password" name="newpassword" ></td>
+          <th class="password">password</th>
+          <td><input class="profile-input" type="password" name="newpassword" ></td>
         </tr>
-        <tr>
+        <tr class="password-form">
           <!--password-->
-          <th>password confirm</th>
-          <td><input type="password" name="newpassword_confirmation"></td>
+          <th class="password-confirm">password confirm</th>
+          <td><input class="profile-input" type="password" name="newpassword_confirmation"></td>
         </tr>
-        <tr>
+        <tr class="bio-form">
           <!--bio-->
           @if($errors->has('bio'))  <!--has()でエラー文の1つを指定して表示できる-->
             {{ $errors->first('bio') }}
           @endif 
-          <th>bio</th>
-          <td><input type="text" name="bio" value="{{$user->bio}}"></td><!--Usercontroller　profileでuser情報を取得してその中からbioのみを取得-->
+          <th class="bio">bio</th>
+          <td><input class="profile-input" type="text" name="bio" value="{{$user->bio}}"></td><!--Usercontroller　profileでuser情報を取得してその中からbioのみを取得-->
         </tr>
-        <tr>
+        <tr class="image-form">
           <!--image-->
           @if($errors->has('iconimage'))  <!--has()でエラー文の1つを指定して表示できる-->
             {{ $errors->first('iconimage') }}
           @endif 
-          <th>icon image</th>
-          <td><input type="file" name="iconimage"></td>
+          <th class="icon-image">icon image</th>
+          <td class="iconimage"><input type="file" name="iconimage"></td>
         </tr> 
       </table>
       <div class="btn-update">
