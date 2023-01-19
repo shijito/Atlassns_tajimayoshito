54fbb143c3253ead57d304ebc17768e6b2267723
shijito <zero.seiyuu@gmail.com>
2023-01-20 03:55:05 +0900

    purofile　下エリア　改

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -2,24 +2,39 @@
 
 @section('content')
 <div class="common-top">
-    <p>Follow List</p>
     <div class="allfollowList">
-        @foreach ($follow_user as $follow_user)
-        <tr>
-            <!--ログインユーザーがフォローした人の一覧を表示する-->
-            <td><img src="{{asset('storage/images/' . $follow_user->images )}}" /><td>
-        <tr>
-        @endforeach
+        <p class="follow-text">Follow List</p>
+        <div class="allfollow-icon">
+            @foreach ($follow_user as $follow_user)
+            <tr>
+                <!--ログインユーザーがフォローした人の一覧を表示する-->
+                <td><img src="{{asset('storage/images/' . $follow_user->images )}}" /><td>
+            <tr>
+            @endforeach
+        </div>
     </div>
 </div>
 
-<div class="followList_timeline">
+<div class="followList-timeline">
     @foreach ($followpost as $followpost)
-    <tr>
-        <td><a href="/follows/{{ $followpost->user->id }}"><img src="{{asset('storage/images/' . $followpost->user->images )}}" /></a><td>
-        <td>{{ $followpost->post }}</td>
-        <td>{{ $followpost->created_at }}</td>
-    </tr>
+    <div class="follow-timeline">
+        <table class="follow-content" >
+            <tr>
+                <td rowspan="2" class="follow-icon" ><a href="/follows/{{ $followpost->user->id }}"><img src="{{asset('storage/images/' . $followpost->user->images )}}" /></a></td>
+                <td class="follow-username">{{ $followpost->user->username }}</td>
+                <td class="follow-tweettime">{{ $followpost->created_at }}</td>
+            </tr>
+            <tr>
+                <td colspan="2" class="follow-tweet">{{ $followpost->post }}</td>
+            </tr>
+            <!-- <tr>
+                <th class="follow-icon" ><a href="/follows/{{ $followpost->user->id }}"><img src="{{asset('storage/images/' . $followpost->user->images )}}" /></a></th>
+                <td class="follow-username">{{ $followpost->user->username }}</td>
+                <td class="follow-tweet">{{ $followpost->post }}</td>
+                <td class="follow-tweettime">{{ $followpost->created_at }}</td>
+            </tr> -->
+        </table>
+    </div>
     @endforeach
 </div>
 
--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -2,21 +2,23 @@
 
 @section('content')
 <div class="common-top">
-    <p>Follower List</p>
     <div class="allfollowList">
-        @foreach ($follower_user as $follower_user)
-            <tr>
-                <!--ログインユーザーのフォロワーの一覧を表示する-->
-                <td><img src="{{asset('storage/images/' . $follower_user->images )}}" /><td>
-            </tr>
-        @endforeach
+        <p class="follow-text">Follower List</p>
+        <div class="class="allfollow-icon">
+            @foreach ($follower_user as $follower_user)
+                <tr>
+                    <!--ログインユーザーのフォロワーの一覧を表示する-->
+                    <td ><img src="{{asset('storage/images/' . $follower_user->images )}}" /><td>
+                </tr>
+            @endforeach
+        </div>
     </div>
 </div>
 
-<div class="followerList_timeline">
+<div class="followList-timeline">
     @foreach ($followerpost as $followerpost)
     <tr>
-        <td><a href="/follows/{{ $followerpost->user->id }}"><img src="{{asset('storage/images/' . $followerpost->user->images )}}" /></a></td>
+        <td><a href="/follows/{{ $followerpost->user->id }}"><img src="{{asset('storage/images/' . $followerpost->user->images )}}"  /></a></td>
         <td>{{ $followerpost->post }}</td>
         <td>{{ $followerpost->created_at }}</td>
     </tr>
