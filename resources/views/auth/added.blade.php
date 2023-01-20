ed6138b9ec4feeb4f92546193b7e2382fd52598d
shijito <zero.seiyuu@gmail.com>
2023-01-21 03:11:44 +0900

    login　css

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -3,12 +3,19 @@
 @section('content')
 
 <div id="clear">
-  <p>{{$name}}さん</p>
-  <p>ようこそ！AtlasSNSへ！</p>
-  <p>ユーザー登録が完了しました。</p>
-  <p>早速ログインをしてみましょう。</p>
-
-  <p class="btn"><a href="/login">ログイン画面へ</a></p>
+  <div class="text-top">
+    <p>{{$name}}さん</p>
+    <p>ようこそ！AtlasSNSへ！</p>
+  </div>
+  
+  <div class="text-bottom">
+    <p>ユーザー登録が完了しました。</p>
+    <p>早速ログインをしてみましょう。</p>
+  </div>
+  
+  <div class="login-btn">
+    <p class="btn"><a href="/login">ログイン画面へ</a></p>
+  </div>
 </div>
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,38 +4,64 @@
 <div  class="register"></div>
 	{!! Form::open() !!}
 
-	<h2>新規ユーザー登録</h2>
+	<h2 class="addatlassns">新規ユーザー登録</h2>
 
 	<!--usernameー-->
 	@if($errors->has('username'))  <!--has()でエラー文の1つを指定して表示できる-->
 		{{ $errors->first('username') }}
 	@endif 
-	{{ Form::label('ユーザー名') }}
-	{{ Form::text('username',null,['class' => 'input']) }}
-
+	
+	<div class="login-username">
+		{{ Form::label('ユーザー名') }}
+	</div>
+	
+	<div class="login-nameform">
+		{{ Form::text('username',null,['class' => 'input']) }}
+	</div>
+	
 	<!--mail-->
 	@if($errors->has('mail'))  <!--has()でエラー文の1つを指定して表示できる-->
 		{{ $errors->first('mail') }}
 	@endif 
-	{{ Form::label('メールアドレス') }}
-	{{ Form::text('mail',null,['class' => 'input']) }}
-
+	
+	<div class="login-addmaillabel">
+		{{ Form::label('メールアドレス') }}
+	</div>
+	
+	<div class="login-addmaillform">
+		{{ Form::text('mail',null,['class' => 'input']) }}
+	</div>
+	
 	<!--password-->
 	@if($errors->has('password'))  <!--has()でエラー文の1つを指定して表示できる-->
 		{{ $errors->first('password') }}
 	@endif 
-	{{ Form::label('パスワード') }}
-	{{ Form::text('password',null,['class' => 'input']) }}
 
-	{{ Form::label('パスワード確認') }}
+	<div class="login-passlabel">
+		{{ Form::label('パスワード') }}
+	</div>
+	
+	<div class="login-passform">
+		{{ Form::text('password',null,['class' => 'input']) }}
+	</div>
+	
+	<div class="login-passlabel">
+		{{ Form::label('パスワード確認') }}
+	</div>
+	
+	<div class="login-passform">
 	{{ Form::text('password_confirmation',null,['class' => 'input']) }}
-
-	{{ Form::submit('登録') }}
-
-	<p><a href="/login">ログイン画面へ戻る</a></p>
-
+	</div>
+	
+	<div class="register-btn">
+		{{ Form::submit('register',['class' => 'registerbtn']) }}
+	</div>
+	
+	<div class="atlassns-add">
+		<p><a href="/login">ログイン画面へ戻る</a></p>
+	</div>
 	{!! Form::close() !!}
 
-
+</div>
 
 @endsection
