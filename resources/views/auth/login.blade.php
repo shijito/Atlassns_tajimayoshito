cefcea2fa7b8a7f383d4f7d874f398e0f04b9a96
shijito <zero.seiyuu@gmail.com>
2023-01-21 01:59:53 +0900

    login css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,18 +6,28 @@
     {!! Form::open() !!}
 
         <p class="atlassns"> AtlasSNSへようこそ</p>
-
-        {{ Form::label('e-mail') }}
-        {{ Form::text('mail',null,['class' => 'input']) }}
-        {{ Form::label('password') }}
-        {{ Form::password('password',['class' => 'input']) }}
-
-        {{ Form::submit('ログイン') }}
-
-        <p><a href="/register">新規ユーザーの方はこちら</a></p>
-
+        <div class="login-maillabel">
+            {{ Form::label('mail adress') }}
+        </div>
+        <div class="login-maillform">
+            {{ Form::text('mail',null,['class' => 'input']) }}
+        </div>
+        <div class="login-passlabel">
+            {{ Form::label('password') }}
+        </div>
+        <div class="login-passform">
+            {{ Form::password('password',['class' => 'input']) }}
+        </div>
+        <div class="login-btn">
+            {{ Form::submit('ログイン',['class' => 'loginbtn']) }}
+        </div>
+        <div class="atlassns-add">
+            <p><a href="/register">新規ユーザーの方はこちら</a></p>
+        </div>
     {!! Form::close() !!}
 </div>
 
 
 @endsection
+
+
