f7418a78aaa70533e8c0062e012fa502b23e416f
shijito <zero.seiyuu@gmail.com>
2023-01-24 08:23:45 +0900

    add css

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -9,11 +9,11 @@
   </div>
   
   <div class="text-bottom">
-    <p>ユーザー登録が完了しました。</p>
-    <p>早速ログインをしてみましょう。</p>
+    <p>ユーザー登録が完了しました。<br>
+       早速ログインをしてみましょう。</p>
   </div>
   
-  <div class="login-btn">
+  <div class="logincomplete-btn">
     <p class="btn"><a href="/login">ログイン画面へ</a></p>
   </div>
 </div>
